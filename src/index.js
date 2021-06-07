c71365753924c4174e3a4fde1c31299ad999d91d
Sean Monaghan <smonagha@conncoll.edu>
2021-06-07 13:04:31 -0400

    added images for the readme

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import './styles/table.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
